8f44ea240b75e50adc34e0ebcd9fa7e70fc90981
hiepcong <beakjoen@gmail.com>
2024-11-23 15:20:45 +0700

    thiết lập ngân sách theo danh mục

--- BudgetManager.java
+++ BudgetManager.java
@@ -10,3 +10,8 @@ public void setBudgetLimit(double limit) {
     budgetLimit = limit;
     System.out.println("Total budget limit set to: " + budgetLimit);
 }
+
+public void setCategoryLimit(String category, double limit) {
+    categoryLimits.put(category, limit);
+    System.out.println("Category " + category + " limit set to: " + limit);
+}
