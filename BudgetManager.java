0348d4c92fba7a13f0c823f9537950bfd8576273
hiepcong <beakjoen@gmail.com>
2024-11-23 15:16:17 +0700

    tạo khung lớp và định nghĩa cơ bản

--- BudgetManager.java
+++ BudgetManager.java
@@ -1,2 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BudgetManager {
+    private double budgetLimit;
+    private final Map<String, Double> categoryLimits = new HashMap<>();
 }
+
--- ReportManager.java
+++ ReportManager.java
@@ -1,2 +1,9 @@
 public class ReportManager {
+    private final TransactionManager transactionManager;
+    private final BudgetManager budgetManager;
+
+    public ReportManager(TransactionManager transactionManager, BudgetManager budgetManager) {
+        this.transactionManager = transactionManager;
+        this.budgetManager = budgetManager;
+    }
 }
