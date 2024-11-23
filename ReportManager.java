8d9ef08c98d951edfce65b9ddecbad555fa6aaad
hiepcong <beakjoen@gmail.com>
2024-11-23 16:10:29 +0700

    Kiểm tra và hoàn chỉnh

--- ReportManager.java
+++ ReportManager.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ReportManager {
     private final TransactionManager transactionManager;
     private final BudgetManager budgetManager;
@@ -5,5 +8,22 @@ public class ReportManager {
     public ReportManager(TransactionManager transactionManager, BudgetManager budgetManager) {
         this.transactionManager = transactionManager;
         this.budgetManager = budgetManager;
+    }
+
+    public void exportReport() {
+        try (FileWriter writer = new FileWriter("Report.txt")) {
+            writer.write("====== BÁO CÁO TÀI CHÍNH ======\n");
+            writer.write("Thu nhập:\n");
+            for (Transaction t : transactionManager.getIncomes()) {
+                writer.write(t + "\n");
+            }
+            writer.write("Chi tiêu:\n");
+            for (Transaction t : transactionManager.getExpenses()) {
+                writer.write(t + "\n");
+            }
+            System.out.println("Báo cáo đã được xuất ra tệp Report.txt.");
+        } catch (IOException e) {
+            System.out.println("Đã xảy ra lỗi khi xuất báo cáo.");
+        }
     }
 }
